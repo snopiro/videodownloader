eee5f0eac60375648d170d8075d933fbf13af18f
snopiro <khoochris13@gmail.com>
2023-07-04 18:56:08 -0600

    path

--- uip.bat
+++ uip.bat
@@ -1,3 +1,3 @@
 @echo off
 set url=%1
-"C:\Users\khooc\AppData\Local\Programs\UiPath\Studio\UiRobot.exe" -file "JavGuruCurlRequest\JavGuruCurlRequest.1.0.6.nupkg" --input "{'in_url' : '%url%'}"
+"pathto\UiPath\Studio\UiRobot.exe" -file "JavGuruCurlRequest\JavGuruCurlRequest.1.0.6.nupkg" --input "{'in_url' : '%url%'}"
